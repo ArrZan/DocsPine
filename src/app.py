4d73a21837ae219b7998b213da8b7091be56afab
DanSantillan <96356971+DanSantillan@users.noreply.github.com>
2024-05-29 00:38:31 -0500

    Update app.py
    Si da conflicto eliminar

--- src/app.py
+++ src/app.py
@@ -6,6 +6,12 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_mysqldb import MySQL
 from flask_wtf.csrf import CSRFProtect
 from apps.login.views import LoginView, RegisterView
+from apps.crear_proyecto.views import crear_proyecto as CrearProyecto
+from apps.crear_proyecto.views import formAddTrabajo as AddTrabajo
+from apps.crear_proyecto.views import delete as Eliminar
+from apps.crear_proyecto.views import editar_proyecto as EditarP
+from apps.crear_proyecto.views import actualizar_proyecto as ActualizarP
+
 
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
@@ -24,6 +30,19 @@ login_manager_app = LoginManager(app)
 STATIC_DIR = os.path.join(app.root_path, 'static') # Dirección de mi static
 
 app.static_folder = 'static'
+# SI DA CONFLICTO ELIMINAR ESTO
+# ******************************************** #
+# Ruta para guardar los archivos en una carpeta
+UPLOAD_FOLDER = os.path.join(app.root_path, 'static', 'archivos_cargados')
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+    print("¡La carpeta para guardar los ARCHIVOS se ha creado correctamente!")
+else:
+    print("La carpeta para guardar los ARCHIVOS ya existe.")
+
+# Configurar la carpeta de carga en la aplicación Flask
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# ******************************************** #
 
 
 @login_manager_app.user_loader
@@ -59,6 +78,32 @@ def home():
 @login_required
 def misProyectos():
     return render_template('proyectos/misProyectos.html')
+
+# Rutas Crear proyecto
+# !------------------------------------------------------------
+# Ruta para mostrar el formulario de creación de proyectos
+@app.route('/crear_proyectos')
+def crear_proyecto():
+    return CrearProyecto()
+
+# Guardar los registros/proyectos
+@app.route('/registrar', methods=['POST'])
+def formAddTrabajo():
+    return AddTrabajo()
+
+@app.route('/delete/<string:ID>')
+def delete(ID):
+    return Eliminar(ID)
+
+# Ruta para editar un proyecto específico mediante su ID
+@app.route('/editar_proyecto/<string:ID>')
+def editar_proyecto(ID):
+    return EditarP(ID)
+    
+# Ruta para actualizar un proyecto después de la edición
+@app.route('/actualizar_proyecto', methods=['POST'])
+def actualizar_proyecto():
+    return ActualizarP()
 
 # Errores de respuesta HTTP
 #------------------------------------------------------------
